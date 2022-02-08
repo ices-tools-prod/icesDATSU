cec128df5dda635f55fb57913a86d8f7098068bc
Colin Millar <colin.millar@ices.dk>
2022-02-08 14:35:04 +0100

    add verbose argument to uploadDatsuFileFireAndForget

--- R/uploadDatsuFileFireAndForget.R
+++ R/uploadDatsuFileFireAndForget.R
@@ -9,18 +9,19 @@
 #' @param sendEmail it is set to TRUE by default, this will specify if the user will to receive an
 #'   email when the session is finished
 #' @param errorLimit does not need to be specified, it is 30000 by default
+#' @param verbose get verbose output from the POST request, default FALSE
 #'
 #' @return The ID of the DATSU session
 #'
 #' @examples
 #'
 #' filename <- system.file("test_files/vms_test.csv", package = "icesDatsu")
-#' uploadDatsuFileFireAndForget(filename, "colin.millar@ices.dk", 145)
+#' uploadDatsuFileFireAndForget(filename, 145)
 #'
 #' @export
 #' @importFrom httr upload_file content
 #' @importFrom jsonlite toJSON
-uploadDatsuFileFireAndForget <- function(fileToUpload, dataSetVerID, emailAddress = NULL, sendEmail = TRUE, errorLimit = 30000) {
+uploadDatsuFileFireAndForget <- function(fileToUpload, dataSetVerID, emailAddress = NULL, sendEmail = TRUE, errorLimit = 30000, verbose = FALSE) {
 
   if (is.null(emailAddress)) {
     emailAddress <- icesConnect::decode_token()$UserEmail
@@ -31,16 +32,16 @@ uploadDatsuFileFireAndForget <- function(fileToUpload, dataSetVerID, emailAddres
     list(
       fileToUpload = upload_file(fileToUpload),
       EmailAddress = emailAddress,
-      DataSetVerID = dataSetVerID,
-      SendEmail = sendEmail,
-      ErrorLimit = errorLimit
+      DataSetVerID = dataSetVerID#,
+#      SendEmail = sendEmail,
+#      ErrorLimit = errorLimit
     )
 
   # perform request
   resp <-
     datsu_post(
       datsu_api("UploadDATSUFileFireAndForget"),
-      body = body, retry = TRUE, verbose = TRUE
+      body = body, retry = TRUE, verbose = verbose
     )
 
   # get results
