73db2fa77da65d33ecc9b7f0b17ded3478a33870
Colin Millar <colin.millar@ices.dk>
2022-02-08 14:58:20 +0100

    * added getMysessionsList()

---
+++ R/getMySessionsList.R
@@ -0,0 +1,17 @@
+#' get screening session list for a user
+#'
+#' This API of the webservice returns a list of the sesisons for the authenticated user.
+#'
+#' @return The details of the users sessions
+#'
+#' @examples
+#'
+#' getMySessionsList()
+#' @export
+getMySessionsList <- function() {
+  url <- datsu_api("getMySessionsList")
+
+  out <- datsu_get(url)
+
+  out
+}
